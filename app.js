05dc627197ae0325a02bb0e216f84b4bdfeb3a40
ashishumrey009 <ashishumrey009@gmail.com>
2023-12-26 15:47:49 +0530

    react class 1

---
+++ app.js
@@ -0,0 +1,7 @@
+const heading = React.createElement("div",{
+    id:"parent"
+},[React.createElement("div",{id:"child"},[
+    React.createElement("h1",{},"I am h1 tag c"),
+    React.createElement("h2",{},"I am h2 tag c"),]),]);
+const root=  ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
